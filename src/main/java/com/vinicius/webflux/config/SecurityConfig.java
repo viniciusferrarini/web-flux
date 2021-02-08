47d7cb7f5deef45acf366a81ae18251a7baf1892
Vinicius Ferrarini <viniciusferrarini@outlook.com>
2021-02-08 16:59:39 -0300

    Implementando security

---
+++ src/main/java/com/vinicius/webflux/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.vinicius.webflux.config;
+
+import com.vinicius.webflux.service.UserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        //@formatter:off
+        return http
+                .csrf().disable()
+                .authorizeExchange()
+                    .pathMatchers(HttpMethod.POST, "/anime/**").hasRole("ADMIN")
+                    .pathMatchers(HttpMethod.GET, "/anime/**").hasRole("USER")
+                .anyExchange().authenticated()
+                .and()
+                    .formLogin()
+                .and()
+                    .httpBasic()
+                .and()
+                    .build();
+        //@formatter:on
+    }
+
+    @Bean
+    ReactiveAuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        return new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
+    }
+
+}
